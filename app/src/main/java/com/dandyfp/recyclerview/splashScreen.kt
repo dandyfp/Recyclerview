f3e8d2f92596193e0021991060ef54a2483c459f
dandy <dandyfp0@gmail.com>
2022-06-25 18:35:59 +0700

    Recyclerview APP

---
+++ app/src/main/java/com/dandyfp/recyclerview/splashScreen.kt
@@ -0,0 +1,19 @@
+package com.dandyfp.recyclerview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class splashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity (Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
